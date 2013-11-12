8b968003ef230d4954a77ef1c4415c9348a98115
Ruling <rulingghost@gmail.com>
2013-11-12 19:15:37 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -10,15 +10,8 @@ else
   export SKIP=0
 fi
 
-clear
-echo "安卓编译环境自动配置脚本"
-echo "作者：Ruling."
-
-if [ ${SKIP} = 1 ]; then
-echo "无人值守安装. 按任意键暂停..."
-else
-read -p "按回车键继续..."
-fi
+echo " 安卓开发环境自动配置脚本 "
+echo " 作者：Rulin "
 
 clear
 
@@ -59,9 +52,9 @@ sudo ln -s /usr/local/python2.5/bin/python /usr/bin/python2.5
 cd ~/Downloads
 
 if [ ${SKIP} = 1 ]; then
-echo "无人值守安装. 按任意键暂停..."
+  echo "无人值守安装. 按任意键暂停..."
 else
-read -p "按回车键继续..."
+  read -p "按回车键继续..."
 fi
 
 clear
@@ -69,34 +62,14 @@ clear
 echo
 echo "安装 CCache!"
 echo
-wget http://www.samba.org/ftp/ccache/ccache-3.1.9.tar.gz
-tar -xvzf ccache-3.1.9.tar.gz
-cd ~/Downloads/ccache-3.1.9
+wget http://www.samba.org/ftp/ccache/ccache-3.1.tar.gz
+tar -xvzf ccache-3.1.tar.gz
+cd ~/Downloads/ccache-3.1
 ./configure
 make -j${JOBS}
 sudo make install -j${JOBS}
 echo "export USE_CCACHE=1" >> ~/.bashrc
-if [ ${SKIP} = 1 ]; then
-echo "无人值守安装. 按任意键暂停..."
-else
-read -p "按回车键继续..."
-fi
-
-clear
-
-echo
-echo "安装 JDK 6!"
-echo
-wget  --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin"
-chmod +x jdk-6u45-linux-x64.bin
-sudo ./jdk-6u45-linux-x64.bin
-sudo mv jdk1.6.0_45 /usr/lib/jvm/jdk1.6.0_45
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.6.0_45/bin/java 1
-sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk1.6.0_45/bin/javac 1
-sudo update-alternatives --install /usr/bin/javaws javaws /usr/lib/jvm/jdk1.6.0_45/bin/javaws 1
-sudo update-alternatives --install /usr/bin/jar jar /usr/lib/jvm/jdk1.6.0_45/bin/jar 1
-sudo update-alternatives --install /usr/bin/javadoc javadoc /usr/lib/jvm/jdk1.6.0_45/bin/javadoc 1
-java -version
+cd ~/Downloads
 
 if [ ${SKIP} = 1 ]; then
 echo "无人值守安装. 按任意键暂停..."
@@ -109,11 +82,30 @@ clear
 echo
 echo "安装 GNU Make!"
 echo
-wget http://ftp.gnu.org/gnu/make/make-3.81.tar.gz
-tar -xvzf make-3.81.tar.gz
-cd ~/Downloads/make-3.81
+wget http://ftp.gnu.org/gnu/make/make-4.0.tar.gz
+tar -xvzf make-4.0.tar.gz
+cd ~/Downloads/make-4.0
 ./configure
 sudo make install -j${JOBS}
+cd ~/
+
+if [ ${SKIP} = 1 ]; then
+echo "无人值守安装. 按任意键暂停..."
+else
+read -p "按回车键继续..."
+fi
+
+clear
+
+echo
+echo "安装 JDK 6!"
+echo
+wget  --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "http://download.oracle.com/otn-pub/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin"
+sudo mv jdk-6u45-linux-x64.bin /usr/
+cd /usr
+sudo chmod 755 jdk-6u45-linux-x64.bin
+sudo ./jdk-6u45-linux-x64.bin
+echo -e '\n# Java\nexport JAVA_HOME=/usr/jdk1.6.0_45\nexport JRE_HOME=/usr/jdk1.6.0_45/jre\nexport PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH:$JAVA_HOME/bin\nexport CLASSPATH=./:$JAVA_HOME/lib:$JAVA_HOME/jre/lib' >> ~/.profile
 cd ~/
 
 if [ ${SKIP} = 1 ]; then
@@ -131,8 +123,9 @@ sudo apt-get install git-core gnupg flex bison gperf build-essential \
 zip curl zlib1g-dev libc6-dev libncurses5-dev x11proto-core-dev \
 libx11-dev libreadline6-dev libgl1-mesa-dev tofrodos python-markdown \
 libxml2-utils xsltproc pngcrush gcc-multilib lib32z1 schedtool \
-libqt4-dev lib32stdc++6 libx11-dev:i386 g++-multilib lib32z1-dev \
-lib32ncurses5-dev ia32-libs mingw32 lib32z-dev $PARAM
+libqt4-gui libqt4-core libqt4-dev lib32stdc++6 libx11-dev:i386 \
+pngcrush schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
+ia32-libs mingw32 lib32z-dev $PARAM
 
 if [ ${SKIP} = 1 ]; then
 echo "无人值守安装. 按任意键暂停..."
@@ -147,17 +140,6 @@ echo "将终端快捷方式加入右键菜单!"
 echo
 sudo apt-get install nautilus-open-terminal $PARAM
 nautilus -q
-
-
-export LANGUAGE=`echo $LANG | grep CN`;
-
-if [[ $LANGUAGE != "" ]]; then
-    echo
-echo "安装 Smarthosts!"
-echo
-sudo wget https://sourceforge.net/projects/androidbuild/files/hosts -O /etc/hosts
-sudo chmod a+x /etc/hosts
-fi;
 
 echo
 echo "安装 GIT!"
@@ -193,7 +175,7 @@ echo "正在下载 Linux 64位 系统的Android SDK"
         wget http://dl.google.com/android/adt/adt-bundle-linux-x86_64-20131030.zip
 echo "下载完成!!"
 echo "展开文件"
-        mkdir ~/adt-bundle
+	mkdir ~/adt-bundle
         mv adt-bundle-linux-x86_64-20131030.zip ~/adt-bundle/adt_x64.zip
         cd ~/adt-bundle
         unzip adt_x64.zip
@@ -201,11 +183,9 @@ echo "展开文件"
 echo "正在配置"
         echo -e '\n# Android tools\nexport PATH=${PATH}:~/adt-bundle/sdk/tools\nexport PATH=${PATH}:~/adt-bundle/sdk/platform-tools\nexport PATH=${PATH}:~/bin' >> ~/.bashrc
         echo -e '\nPATH="$HOME/adt-bundle/sdk/tools:$HOME/adt-bundle/sdk/platform-tools:$PATH"' >> ~/.profile
-        echo "创建桌面快捷方式"
-        ln -s ~/adt-bundle/eclipse/eclipse ~/Desktop/Eclipse
-        ln -s ~/adt-bundle/sdk/tools/android ~/Desktop/SDK-Manager
 echo "完成!!"
 else
+
 echo
 echo "正在下载 Linux 32位 系统的Android SDK"
         wget http://dl.google.com/android/adt/adt-bundle-linux-x86-20131030.zip
@@ -219,9 +199,6 @@ echo "展开文件"
 echo "正在配置"
         echo -e '\n# Android tools\nexport PATH=${PATH}:~/adt-bundle/sdk/tools\nexport PATH=${PATH}:~/adt-bundle/sdk/platform-tools\nexport PATH=${PATH}:~/bin' >> ~/.bashrc
         echo -e '\nPATH="$HOME/adt-bundle/sdk/tools:$HOME/adt-bundle/sdk/platform-tools:$PATH"' >> ~/.profile
-        echo "创建桌面快捷方式"
-        ln -s ~/adt-bundle/eclipse/eclipse ~/Desktop/Eclipse
-        ln -s ~/adt-bundle/sdk/tools/android ~/Desktop/SDK-Manager
 echo "完成!!"
 fi
 
@@ -234,25 +211,67 @@ fi
 clear
 
 echo
+echo "安装 APKTool"
+echo
+wget http://android-apktool.googlecode.com/files/apktool1.5.2.tar.bz2
+tar -jxf apktool1.5.2.tar.bz2
+mv -f apktool1.5.2/apktool.jar ~/bin/apktool.jar
+wget http://android-apktool.googlecode.com/files/apktool-install-linux-r05-ibot.tar.bz2
+tar -jxf apktool-install-linux-r05-ibot.tar.bz2
+mv -f apktool-install-linux-r05-ibot/aapt ~/bin/aapt
+mv -f apktool-install-linux-r05-ibot/apktool ~/bin/apktool
+
+if [ ${SKIP} = 1 ]; then
+echo "无人值守安装. 按任意键暂停..."
+else
+read -p "按回车键继续..."
+fi
+
+clear
+
+echo
+echo "安装 安卓厨房"
+echo
+wget https://github.com/dsixda/Android-Kitchen/archive/master.zip
+unzip master.zip
+mv -f Android-Kitchen-master ~/Android-Kitchen
+echo -e '\n#!/bin/bash\ncd ~/Android-Kitchen\n./menu' >> ~/Android-Kitchen/kitchen
+chmod 755 ~/Android-Kitchen/kitchen
+ln -s ~/Android-Kitchen/kitchen ~/bin/kitchen
+ln -s ~/Android-Kitchen/kitchen ~/桌面/Android-Kitchen
+
+if [ ${SKIP} = 1 ]; then
+echo "无人值守安装. 按任意键暂停..."
+else
+read -p "按回车键继续..."
+fi
+
+clear
+
+echo
 echo "清除临时文件..."
 echo
-rm -f ~/Downloads/Python-2.5.6.tgz
-sudo chmod 777 ~/Downloads/Python-2.5.6/Lib/plat-linux3
-rm -Rf ~/Downloads/Python-2.5.6
-rm -f ~/Downloads/make-3.81.tar.gz
-rm -Rf ~/Downloads/make-3.81
-rm -f ~/Downloads/jdk-6u45-linux-x64.bin
-rm -f ~/Downloads/ccache-3.1.9.tar.gz
-rm -Rf ~/Downloads/ccache-3.1.9
+rm -f ~/Downloads/make-4.0.tar.gz
+rm -Rf ~/Downloads/make-4.0
+rm -f ~/jdk-6u45-linux-x64.bin
+rm -f ~/Downloads/ccache-3.1.tar.gz
+rm -Rf ~/Downloads/ccache-3.1
 rm -Rf ~/adt-bundle/adt-bundle-linux-x86_64-20131030
 rm -Rf ~/adt-bundle/adt-bundle-linux-x86-20131030
 rm -f ~/adt-bundle/adt_x64.zip
 rm -f ~/adt-bundle/adt_x86.zip
+rm -f ~/Downloads/master.zip
+rm -f ~/Downloads/apktool1.5.2.tar.bz2
+rm -Rf ~/Downloads/apktool1.5.2
+rm -f ~/Downloads/apktool-install-linux-r05-ibot.tar.bz2
+rm -Rf ~/Downloads/apktool-install-linux-r05-ibot
+
+clear
 
 echo
 echo "完成!"
 echo
-echo "感谢使用本脚本!"
-echo "现在，享受一下编译 roms/kernels 吧:)"
+echo "感谢使用本的脚本!"
+echo
 read -p "按回车键退出..."
 exit
